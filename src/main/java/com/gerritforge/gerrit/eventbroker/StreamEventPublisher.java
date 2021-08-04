8d47564c0981123b3b73c97e842dd7e46cf3b6b5
Marcin Czech <maczech@gmail.com>
2021-08-04 15:00:42 +0200

    Do not publish forwarded stream events
    StreamEventPublisher uses gerrit.instanceId and Event.instanceId to
    decide if the event is generated locally or forwarded from other node.
    All events forwarded from other nodes or events without instanceId are
    not published. This allows to avoid message duplication.
    
    Bug: Issue 14852
    Change-Id: I3cc8053a546f3f95c2f47b658a36b7dd91fb8e85

--- src/main/java/com/gerritforge/gerrit/eventbroker/StreamEventPublisher.java
+++ src/main/java/com/gerritforge/gerrit/eventbroker/StreamEventPublisher.java
@@ -15,6 +15,7 @@
 package com.gerritforge.gerrit.eventbroker;
 
 import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.server.config.GerritInstanceId;
 import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.EventListener;
 import com.google.inject.Inject;
@@ -26,18 +27,22 @@ public class StreamEventPublisher implements EventListener {
   public static final String STREAM_EVENTS_TOPIC = "stream_events_topic";
   private final DynamicItem<BrokerApi> brokerApiDynamicItem;
   private final String streamEventsTopic;
+  private final String instanceId;
 
   @Inject
   public StreamEventPublisher(
-      DynamicItem<BrokerApi> brokerApi, @Named(STREAM_EVENTS_TOPIC) String streamEventsTopic) {
+      DynamicItem<BrokerApi> brokerApi,
+      @Named(STREAM_EVENTS_TOPIC) String streamEventsTopic,
+      @GerritInstanceId String instanceId) {
     this.brokerApiDynamicItem = brokerApi;
     this.streamEventsTopic = streamEventsTopic;
+    this.instanceId = instanceId;
   }
 
   @Override
   public void onEvent(Event event) {
     BrokerApi brokerApi = brokerApiDynamicItem.get();
-    if (brokerApi != null) {
+    if (brokerApi != null && instanceId.equals(event.instanceId)) {
       brokerApi.send(streamEventsTopic, event);
     }
   }
