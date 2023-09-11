80d5fcc7f0ef39d4b634922cf7c06b75ce3f2f06
Luca Milanesio <luca.milanesio@gmail.com>
2023-09-11 22:10:49 +0100

    Remove actual implementation of InProcessBrokerApi
    The InProcessBrokerApi main purpose was to allow to register
    the events listeners independently from the actual implementation
    of the broker-api.
    
    The implementation went unfortunately well beyond the initial scope
    and started to simulate a full in-process broker implementation
    which would not make sense to use in production.
    Also, the fact of silently mocking the real behaviour was potentially
    leaving configuration and setup issues unnoticed with the drawback
    of not notifing the Gerrit admins that something wasn't quite right.
    
    Throw an exception of unsupported method for all of those calls
    that are not supposed to be used as actual implementation of a
    broker.
    
    Change-Id: I92100d3d447aba32af58cd05e065affb402b7372

--- src/main/java/com/gerritforge/gerrit/eventbroker/InProcessBrokerApi.java
+++ src/main/java/com/gerritforge/gerrit/eventbroker/InProcessBrokerApi.java
@@ -16,64 +16,30 @@ package com.gerritforge.gerrit.eventbroker;
 
 import static com.gerritforge.gerrit.eventbroker.TopicSubscriber.topicSubscriber;
 
-import com.google.common.collect.EvictingQueue;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.MapMaker;
-import com.google.common.eventbus.EventBus;
-import com.google.common.eventbus.Subscribe;
 import com.google.common.flogger.FluentLogger;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.SettableFuture;
 import com.google.gerrit.server.events.Event;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public class InProcessBrokerApi implements BrokerApi {
   private static final FluentLogger log = FluentLogger.forEnclosingClass();
-
-  private static final Integer DEFAULT_MESSAGE_QUEUE_SIZE = 100;
-
-  private final Map<String, EvictingQueue<Event>> messagesQueueMap;
-  private final Map<String, EventBus> eventBusMap;
   private final Set<TopicSubscriber> topicSubscribers;
 
   public InProcessBrokerApi() {
-    this.eventBusMap = new MapMaker().concurrencyLevel(1).makeMap();
-    this.messagesQueueMap = new MapMaker().concurrencyLevel(1).makeMap();
     this.topicSubscribers = new HashSet<>();
   }
 
   @Override
   public ListenableFuture<Boolean> send(String topic, Event message) {
-    EventBus topicEventConsumers = eventBusMap.get(topic);
-    SettableFuture<Boolean> future = SettableFuture.create();
-
-    if (topicEventConsumers != null) {
-      topicEventConsumers.post(message);
-      future.set(true);
-    } else {
-      future.set(false);
-    }
-
-    return future;
+    return unsupported();
   }
 
   @Override
   public void receiveAsync(String topic, Consumer<Event> eventConsumer) {
-    EventBus topicEventConsumers = eventBusMap.get(topic);
-    if (topicEventConsumers == null) {
-      topicEventConsumers = new EventBus(topic);
-      eventBusMap.put(topic, topicEventConsumers);
-    }
-
-    topicEventConsumers.register(eventConsumer);
     topicSubscribers.add(topicSubscriber(topic, eventConsumer));
-
-    EvictingQueue<Event> messageQueue = EvictingQueue.create(DEFAULT_MESSAGE_QUEUE_SIZE);
-    messagesQueueMap.put(topic, messageQueue);
-    topicEventConsumers.register(new EventBusMessageRecorder(messageQueue));
   }
 
   @Override
@@ -83,28 +49,16 @@ public class InProcessBrokerApi implements BrokerApi {
 
   @Override
   public void disconnect() {
-    this.eventBusMap.clear();
+    this.topicSubscribers.clear();
   }
 
   @Override
   public void replayAllEvents(String topic) {
-    if (messagesQueueMap.containsKey(topic)) {
-      messagesQueueMap.get(topic).stream().forEach(eventMessage -> send(topic, eventMessage));
-    }
+    unsupported();
   }
 
-  private static class EventBusMessageRecorder {
-    private EvictingQueue messagesQueue;
-
-    public EventBusMessageRecorder(EvictingQueue messagesQueue) {
-      this.messagesQueue = messagesQueue;
-    }
-
-    @Subscribe
-    public void recordCustomerChange(Event e) {
-      if (!messagesQueue.contains(e)) {
-        messagesQueue.add(e);
-      }
-    }
+  private <T> T unsupported() {
+    throw new UnsupportedOperationException(
+        "InProcessBrokerApi is not intended to be used as a real broker");
   }
 }
