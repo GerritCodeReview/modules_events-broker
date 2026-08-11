49fa7e04059922f49b6ac392af305e57f55d147e
Daniele Sassoli <danielesassoli@gmail.com>
2026-08-11 16:32:30 +0100

    Notify listeners when broker implementations are bound
    This allows consumers to subscribe only when a real broker
    implementation is bound. Consumers should wait to subscribe until a real
    broker is bound as otherwise it could lead to failures to instantiate
    consumers.
    
    Signal to consumers when a real broker is bound so they subscribe
    correctly.
    
    Bug: Issue 543285477
    Change-Id: Ia3d99d8af613ac2638209ac24f95adb7fb3e83aa

---
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiPluginListener.java
@@ -0,0 +1,84 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.eventbroker;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.extensions.registration.PluginName;
+import com.google.gerrit.server.plugins.Plugin;
+import com.google.gerrit.server.plugins.StartPluginListener;
+import com.google.gerrit.server.plugins.StopPluginListener;
+import java.util.Optional;
+
+/**
+ * Reacts to the {@link BrokerApi} bound into a {@link DynamicItem} being provided or withdrawn by a
+ * plugin.
+ *
+ * <p>Implementations bind themselves as {@link StartPluginListener} and {@link StopPluginListener}
+ * and are called back through {@link #onBrokerApiBound()} and {@link #onBrokerApiUnbound()} only
+ * for the plugin that provides the {@link BrokerApi}.
+ *
+ * <p>Plugin reload is deliberately not handled: reloading a plugin does not unload it first, so the
+ * state of a broker plugin across a reload is undefined and is not supported.
+ *
+ * <p>No callback is fired for a broker plugin that was already loaded when the implementation was
+ * registered, so implementations must also consult {@link #isBrokerApiBound()} on startup.
+ */
+public interface BrokerApiPluginListener extends StartPluginListener, StopPluginListener {
+  FluentLogger log = FluentLogger.forEnclosingClass();
+
+  /** Returns the item a broker plugin binds its {@link BrokerApi} into. */
+  DynamicItem<BrokerApi> brokerApiDynamicItem();
+
+  /** Returns the name of the broker plugin currently being consumed from, if any. */
+  Optional<String> subscribedBrokerApiPlugin();
+
+  void onBrokerApiBound();
+
+  void onBrokerApiUnbound();
+
+  default boolean isBrokerApiBound() {
+    DynamicItem<BrokerApi> item = brokerApiDynamicItem();
+    return !PluginName.GERRIT.equals(item.getPluginName()) && item.get() != null;
+  }
+
+  @Override
+  default void onStartPlugin(Plugin plugin) {
+    if (bindsBrokerApi(plugin)) {
+      onBrokerApiBound();
+    }
+  }
+
+  @Override
+  default void onStopPlugin(Plugin plugin) {
+    if (providesSubscribedBrokerApi(plugin)) {
+      try {
+        onBrokerApiUnbound();
+      } catch (RuntimeException e) {
+        log.atSevere().withCause(e).log(
+            "Listener failed to react to the BrokerApi of plugin %s being unbound",
+            plugin.getName());
+      }
+    }
+  }
+
+  private boolean bindsBrokerApi(Plugin plugin) {
+    return plugin.getName().equals(brokerApiDynamicItem().getPluginName());
+  }
+
+  private boolean providesSubscribedBrokerApi(Plugin plugin) {
+    return subscribedBrokerApiPlugin().filter(plugin.getName()::equals).isPresent();
+  }
+}
