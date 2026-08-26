21e987364b5b16ad23c02b995a7bd531eee2cdaf
Daniele Sassoli <danielesassoli@gmail.com>
2026-08-26 09:53:19 +0100

    Add BrokerApiLoggingWrapper logging the events sent to the broker
    Provide a BrokerApi delegate that logs to the message log the events
    successfully sent to the broker, and the ones received from it, so that
    consumers of the API do not have to log them themselves.
    
    The events sent are logged as PUBLISH, unless the caller asks for a
    different direction, as it is the case for the events requeued by
    multi-site. The events received are logged as CONSUME as soon as the
    broker hands them over, hence also the ones that the subscribers end up
    dropping.
    
    Change-Id: I676846bdcab8eae74668d2141b845c5be40f9aa9

---
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiLoggingWrapper.java
@@ -0,0 +1,129 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.eventbroker;
+
+import com.gerritforge.gerrit.eventbroker.log.MessageLogger;
+import com.google.common.flogger.FluentLogger;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.server.events.Event;
+import com.google.inject.Inject;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+/** {@link BrokerApi} delegate that logs to the message log the events sent to the broker. */
+public class BrokerApiLoggingWrapper implements BrokerApi {
+  private static final FluentLogger log = FluentLogger.forEnclosingClass();
+
+  private final DynamicItem<BrokerApi> apiDelegate;
+  private final MessageLogger msgLog;
+
+  @Inject
+  public BrokerApiLoggingWrapper(DynamicItem<BrokerApi> apiDelegate, MessageLogger msgLog) {
+    this.apiDelegate = apiDelegate;
+    this.msgLog = msgLog;
+  }
+
+  @Override
+  public ListenableFuture<Boolean> send(String topic, Event message) {
+    return send(topic, message, MessageLogger.Direction.PUBLISH);
+  }
+
+  protected ListenableFuture<Boolean> send(
+      String topic, Event message, MessageLogger.Direction direction) {
+    ListenableFuture<Boolean> resultF = delegate().send(topic, message);
+    resultF.addListener(
+        () -> {
+          try {
+            if (Futures.getDone(resultF)) {
+              msgLog.log(direction, topic, message);
+            }
+          } catch (ExecutionException | RuntimeException | Error e) {
+            logSendFailure(
+                e instanceof ExecutionException ? e.getCause() : e, direction, topic, message);
+          }
+        },
+        MoreExecutors.directExecutor());
+
+    return resultF;
+  }
+
+  private static void logSendFailure(
+      Throwable throwable, MessageLogger.Direction direction, String topic, Event message) {
+    log.atSevere().withCause(throwable).log(
+        "Failed to %s message '%s' to topic '%s'", direction, message, topic);
+  }
+
+  private BrokerApi delegate() {
+    return apiDelegate.get();
+  }
+
+  @Override
+  public void receiveAsync(String topic, AckAwareConsumer<Event> consumer) {
+    delegate().receiveAsync(topic, loggingConsumer(topic, consumer));
+  }
+
+  @Override
+  public void receiveAsync(String topic, String groupId, AckAwareConsumer<Event> consumer) {
+    delegate().receiveAsync(topic, groupId, loggingConsumer(topic, consumer));
+  }
+
+  @Override
+  public void receiveAsyncWithPartition(
+      String topic, String partition, String groupId, AckAwareConsumer<Event> consumer) {
+    delegate()
+        .receiveAsyncWithPartition(topic, partition, groupId, loggingConsumer(topic, consumer));
+  }
+
+  private AckAwareConsumer<Event> loggingConsumer(String topic, AckAwareConsumer<Event> consumer) {
+    return (event, acknowledgement) -> {
+      msgLog.log(MessageLogger.Direction.CONSUME, topic, event);
+      consumer.accept(event, acknowledgement);
+    };
+  }
+
+  @Override
+  public void disconnect() {
+    delegate().disconnect();
+  }
+
+  @Override
+  public void disconnect(String topic, @Nullable String groupId) {
+    delegate().disconnect(topic, groupId);
+  }
+
+  @Override
+  public Set<TopicSubscriber> topicSubscribers() {
+    return delegate().topicSubscribers();
+  }
+
+  @Override
+  public Set<TopicSubscriberWithGroupId> topicSubscribersWithGroupId() {
+    return delegate().topicSubscribersWithGroupId();
+  }
+
+  @Override
+  public void replayAllEvents(String topic) {
+    delegate().replayAllEvents(topic);
+  }
+
+  @Override
+  public boolean isAutoAck() {
+    return delegate().isAutoAck();
+  }
+}
