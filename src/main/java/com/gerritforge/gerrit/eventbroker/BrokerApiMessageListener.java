7943d38247d22f85cd9678f39dd96fb96c9f50b6
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-09 20:43:14 +0100

    Allow registering listeners to message processing
    Allow an external listener to be notified whenever a message
    is sent, requeued or received from a BrokerApi implementation.
    
    Only one listener is active at a time; when registering a listener
    the existing listener is returned, allowing chaining or replacing
    a listener with another one.
    
    Change-Id: Idbe8acac507ad50cf60f49be28aa36738fe08958

---
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiMessageListener.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.eventbroker;
+
+import com.gerritforge.gerrit.eventbroker.log.MessageLogger;
+
+/** API for sending/receiving events through a message Broker. */
+public interface BrokerApiMessageListener {
+
+  /**
+   * Message has been processed to/from a topic successfully.
+   *
+   * @param topic topic name
+   * @param message sent/requeued/received to/from the topic
+   */
+  void messageProcessed(MessageLogger.Direction direction, String topic, Object message);
+
+  /**
+   * Message failed to be processed to/from a topic.
+   *
+   * @param topic topic name
+   * @param message sent/requeued/received failed to/from the topic
+   * @param e exception that was raised when the message failed
+   */
+  void messageFailed(MessageLogger.Direction direction, String topic, Object message, Throwable e);
+}
