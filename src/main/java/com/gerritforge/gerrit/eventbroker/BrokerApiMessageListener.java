f21fb844dbcf840f1699935522c4dc8eb4f94528
Daniele Sassoli <danielesassoli@gmail.com>
2026-09-10 15:46:44 +0100

    Don't gson encode payloads before logging them
    The events we read off the broker would have already been serialised
    before they where published, so we should only log what we read, without
    the need to gson encode it again.
    
    Change-Id: I74c6ed54e4b79e72ea58c9d1714bb542838c3394

--- src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiMessageListener.java
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiMessageListener.java
@@ -15,7 +15,6 @@
 package com.gerritforge.gerrit.eventbroker;
 
 import com.gerritforge.gerrit.eventbroker.log.MessageLogger;
-import com.google.gerrit.server.events.Event;
 
 /** API for sending/receiving events through a message Broker. */
 public interface BrokerApiMessageListener {
@@ -36,17 +35,17 @@ public interface BrokerApiMessageListener {
    *
    * @param direction whether the message was published, requeued or consumed
    * @param topic topic name
-   * @param message event sent/requeued/received to/from the topic
+   * @param message serialized payload sent/requeued/received to/from the topic
    */
-  void messageProcessed(MessageLogger.Direction direction, String topic, Event message);
+  void messageProcessed(MessageLogger.Direction direction, String topic, String message);
 
   /**
    * Message failed to be processed to/from a topic.
    *
    * @param direction whether the message was published, requeued or consumed
    * @param topic topic name
-   * @param message event that failed to be sent/requeued/received to/from the topic
+   * @param message serialized payload that failed to be sent/requeued/received to/from the topic
    * @param e exception that was raised when the message failed
    */
-  void messageFailed(MessageLogger.Direction direction, String topic, Event message, Throwable e);
+  void messageFailed(MessageLogger.Direction direction, String topic, String message, Throwable e);
 }
