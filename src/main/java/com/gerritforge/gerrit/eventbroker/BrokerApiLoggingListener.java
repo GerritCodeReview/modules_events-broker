fc29dc0bdb88ba81a297bf065d7d6efee92cbe54
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-10 06:31:44 +0100

    Introduce a logging listener for BrokerApi implementations
    Let broker implementations report each processed message to the logging
    message listener, so that consumers of the messages don't have to log
    them themselves.
    
    StreamEventPublisher no longer logs the events it publishes, as the
    broker now reports them, consequently the StreamEventPublisherTest does
    not assert anything about logging anymore.
    
    Change-Id: I5253b572c301a52ff162829659c82b87e6196fb7

---
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiLoggingListener.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.eventbroker;
+
+import com.gerritforge.gerrit.eventbroker.log.MessageLogger;
+import com.google.common.flogger.FluentLogger;
+import com.google.inject.Inject;
+
+/** {@link BrokerApi} delegates that logs to the message logs the events sent to the broker. */
+public class BrokerApiLoggingListener implements BrokerApiMessageListener {
+  private static final FluentLogger log = FluentLogger.forEnclosingClass();
+
+  private final MessageLogger msgLog;
+
+  @Inject
+  public BrokerApiLoggingListener(MessageLogger msgLog) {
+    this.msgLog = msgLog;
+  }
+
+  @Override
+  public void messageProcessed(MessageLogger.Direction direction, String topic, Object message) {
+    msgLog.log(direction, topic, message);
+  }
+
+  @Override
+  public void messageFailed(
+      MessageLogger.Direction direction, String topic, Object message, Throwable e) {
+    log.atSevere().withCause(e).log(
+        "Failed to %s message '%s' to topic '%s'", direction, message, topic);
+  }
+}
