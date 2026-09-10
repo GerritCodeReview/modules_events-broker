fbe6621c9a6814b186e240cc2d5c83ff200dbbef
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-10 06:31:44 +0100

    Introduce a logging listener for BrokerApi implementations
    Let broker implementations report each processed message to the logging message listener,
    so that consumers of the messages don't have to log them themselves.
    
    StreamEventPublisher no longer logs the events it publishes, as the
    broker now reports them.
    
    Change-Id: I5253b572c301a52ff162829659c82b87e6196fb7

--- src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiMessageListener.java
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiMessageListener.java
@@ -19,20 +19,33 @@ import com.gerritforge.gerrit.eventbroker.log.MessageLogger;
 /** API for sending/receiving events through a message Broker. */
 public interface BrokerApiMessageListener {
 
+  BrokerApiMessageListener NOOP_LISTENER =
+      new BrokerApiMessageListener() {
+        @Override
+        public void messageProcessed(
+            MessageLogger.Direction direction, String topic, String message) {}
+
+        @Override
+        public void messageFailed(
+            MessageLogger.Direction direction, String topic, String message, Throwable e) {}
+      };
+
   /**
    * Message has been processed to/from a topic successfully.
    *
+   * @param direction whether the message was published, requeued or consumed
    * @param topic topic name
-   * @param message sent/requeued/received to/from the topic
+   * @param message serialized payload sent/requeued/received to/from the topic
    */
-  void messageProcessed(MessageLogger.Direction direction, String topic, Object message);
+  void messageProcessed(MessageLogger.Direction direction, String topic, String message);
 
   /**
    * Message failed to be processed to/from a topic.
    *
+   * @param direction whether the message was published, requeued or consumed
    * @param topic topic name
-   * @param message sent/requeued/received failed to/from the topic
+   * @param message serialized payload that failed to be sent/requeued/received to/from the topic
    * @param e exception that was raised when the message failed
    */
-  void messageFailed(MessageLogger.Direction direction, String topic, Object message, Throwable e);
+  void messageFailed(MessageLogger.Direction direction, String topic, String message, Throwable e);
 }
