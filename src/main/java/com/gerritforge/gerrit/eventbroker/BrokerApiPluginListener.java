2509c6d9a7e6d816a85a675478400264f17462dd
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-11 13:57:50 +0100

    Include the plugin stop lifecycle with the BrokerApiPluginListener
    Allow plugins that are consuming a BrokerApi to be notified when a
    plugin providing the DynamicItem<> is stopped or unloaded.
    
    NOTE: The additional listener won't fire for plugin reloads.
    Change-Id: Id20cec60467c972010f4415541617e229890d523

--- src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiPluginListener.java
+++ src/main/java/com/gerritforge/gerrit/eventbroker/BrokerApiPluginListener.java
@@ -17,6 +17,7 @@ package com.gerritforge.gerrit.eventbroker;
 import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.server.plugins.Plugin;
 import com.google.gerrit.server.plugins.StartPluginListener;
+import com.google.gerrit.server.plugins.StopPluginListener;
 
 /**
  * Reacts to the {@link BrokerApi} bound into a {@link DynamicItem} being provided by a plugin.
@@ -30,12 +31,14 @@ import com.google.gerrit.server.plugins.StartPluginListener;
  * <p>No callback is fired for a broker plugin that was already loaded when the implementation was
  * registered, so implementations must also consult {@link #isBrokerApiStarted()} on startup.
  */
-public interface BrokerApiPluginListener extends StartPluginListener {
+public interface BrokerApiPluginListener extends StartPluginListener, StopPluginListener {
 
   /** Returns the item a broker plugin binds its {@link BrokerApi} into. */
   DynamicItem<BrokerApi> brokerApiDynamicItem();
 
   void onBrokerApiStarted();
+
+  void onBrokerApiStopped();
 
   default boolean isBrokerApiStarted() {
     DynamicItem<BrokerApi> item = brokerApiDynamicItem();
@@ -46,6 +49,13 @@ public interface BrokerApiPluginListener extends StartPluginListener {
   default void onStartPlugin(Plugin plugin) {
     if (bindsBrokerApi(plugin)) {
       onBrokerApiStarted();
+    }
+  }
+
+  @Override
+  default void onStopPlugin(Plugin plugin) {
+    if (bindsBrokerApi(plugin)) {
+      onBrokerApiStopped();
     }
   }
 
